5e9fd679e41721bc6c9ac134e7cf46af0a51987d
niks0101 <niks9289.bomble@gmail.com>
2017-05-03 18:33:16 +0530

    Update urls.py

--- django_angular-master/projects/urls.py
+++ django_angular-master/projects/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from projects import views
 
 urlpatterns = [
-    url(r'^projects/$', views.project_list),
+    url(r'^projects/user/(?P<pk>[0-9]+)/$', views.project_list),
     url(r'^projects/(?P<pk>[0-9]+)/$', views.project_detail),
     url(r'^questionset/(?P<pk>[0-9]+)/$', views.questionset_detail),
 ]
